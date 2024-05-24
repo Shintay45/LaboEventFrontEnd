52d85ca781e58a7b9bbcb0cf50a838fc632fe7de
b.deluca <b.deluca@GOS-VDI908>
2024-05-24 11:11:37 +0200

    Ajoutez des fichiers projet.

---
+++ LaboEventFrontEnd/Pages/RegisterForm.razor.cs
@@ -0,0 +1,20 @@
+﻿using LaboEventFrontEnd.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace LaboEventFrontEnd.Pages
+{
+    public partial class RegisterForm
+    {
+        public RegisterUserForm MyForm { get; set; }
+
+        protected override void OnInitialized()
+        {
+            MyForm = new RegisterUserForm();
+        }
+        public async Task OnSubmitForm()
+        {
+            
+        }
+
+    }
+}
