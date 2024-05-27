60c59aa3a4d42e6966c453f433ccacf2ae52be10
surcula <david.gaillet@live.fr>
2024-05-27 10:12:26 +0200

    Ajout Toasts

--- LaboEventFrontEnd/Program.cs
+++ LaboEventFrontEnd/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.Toast;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -12,6 +13,7 @@ namespace LaboEventFrontEnd
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7080/Api/") });
+            builder.Services.AddBlazoredToast();
 
             await builder.Build().RunAsync();
         }
