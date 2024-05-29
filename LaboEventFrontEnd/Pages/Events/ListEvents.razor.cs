76414805026498be0576e28cbb6c5e131b41e6ca
david gaillet <david.gaillet@live.fr>
2024-05-29 16:57:49 +0200

    update event

---
+++ LaboEventFrontEnd/Pages/Events/ListEvents.razor.cs
@@ -0,0 +1,39 @@
+﻿
+using Blazored.Toast.Services;
+using LaboEventFrontEnd.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace LaboEventFrontEnd.Pages.Events
+{
+    public partial class ListEvents
+    {
+        [Inject]
+        public HttpClient Client { get; set; }
+        [Inject]
+        public IToastService ToastService { get; set; }
+        [Inject]
+        public NavigationManager Nav { get; set; }
+
+        public List<EventComplete> ListEvent { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                HttpResponseMessage response = await Client.GetAsync("Event");
+                if (response.IsSuccessStatusCode)
+                {
+                    
+                }
+                else
+                {
+                    ToastService.ShowError("Une erreur est survenue.");
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                ToastService.ShowError(ex.Message);
+            }
+        }
+    }
+}
